9400685bd2a3c520a4469858a511366cc1ec4610
RihamRamzi <rihamsoftware@gmail.com>
2024-11-13 07:50:20 +0530

    Created basic calculator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator;
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
+}
