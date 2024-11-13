dd9dd56800ec0cb5235924a91df129fb85578d72
RihamRamzi <rihamsoftware@gmail.com>
2024-11-13 16:58:02 +0530

    created function for number buttons
    1. used foreach method and added eventListeners
    to the number button and store the output
    in a variable

--- script.js
+++ script.js
@@ -11,10 +11,26 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-let num1 = [];
-let num2 = 0;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
 
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
+
+const numbers = document.querySelector(".numbers");
+const buttons = document.querySelectorAll("button");
+const calDisplay = document.querySelector(".display");
+
+buttons.forEach((button) => {
+  if (/\d/.test(button.textContent)) {
+    button.addEventListener("click", () => {
+      calDisplay.textContent += button.textContent;
+      num1 = calDisplay.textContent;
+      console.log(num1);
+    });
+
+    console.log(button);
+  }
+});
