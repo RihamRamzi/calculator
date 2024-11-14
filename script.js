fecca39128d649669215811bb8e98b7e34dbf8cf
RihamRamzi <rihamsoftware@gmail.com>
2024-11-14 06:27:38 +0530

    completed the task on storing the values
    1.store num1 , operator and num2 in var so we can
    use it to perfor the calculations

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function divide(num1, num2) {
 let num1 = "";
 let num2 = "";
 let operator = "";
+let isNum2 = false;
 
 function operate(operator, num1, num2) {
   return operator(num1, num2);
@@ -24,13 +25,32 @@ const buttons = document.querySelectorAll("button");
 const calDisplay = document.querySelector(".display");
 
 buttons.forEach((button) => {
+  const operators = ["÷", "×", "-", "+"];
+
+  //numButtons
   if (/\d/.test(button.textContent)) {
     button.addEventListener("click", () => {
-      calDisplay.textContent += button.textContent;
-      num1 = calDisplay.textContent;
-      console.log(num1);
+      if (isNum2) {
+        calDisplay.textContent += button.textContent;
+        num2 = calDisplay.textContent;
+      } else {
+        calDisplay.textContent += button.textContent;
+        num1 = calDisplay.textContent;
+      }
     });
+  } else if (operators.includes(button.textContent)) {
+    //operators
+    if (button.textContent === "+") {
+      button.addEventListener("click", () => {
+        console.log(num1);
 
-    console.log(button);
+        operator = button.textContent;
+        console.log(operator);
+        console.log(num2);
+
+        calDisplay.textContent = "";
+        isNum2 = true;
+      });
+    }
   }
 });
